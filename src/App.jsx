04e00d3d741f21aa9103b986d81ab577e9fdca81
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-24 13:46:40 +0900

    feat: 가구 아이템 클릭시 가구 선택 모달창 띄우는 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,63 @@
 import "./App.scss";
 import ImageMap from "image-map";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import bed_default from "./assets/img/bed_default.png";
+import SelectModal from "./components/SelectModal";
 
 function App() {
-  useEffect(() => {
-    ImageMap("img[usemap]");
-  }, []);
+    const [isSelectModalOn, setSelectModalOn] = useState(false);
+    const [selectedItem, setSelectedItem] = useState("");
+    useEffect(() => {
+        ImageMap("img[usemap]");
+    }, []);
 
-  return (
-    <div className="App">
-      <div className="AppWrap">
-        MYAPP
-        <div className="imgWrap">
-          <div className="roomBgImg" />
+    const onClickItem = (e, item) => {
+        e.preventDefault();
 
-          {/* <div className="bedDefaultImg" useMap="#image-map" /> */}
-          <img src={bed_default} usemap="#image-map" />
-          <map name="image-map">
-            <area
-              onClick={() => alert("bed")}
-              target=""
-              alt=""
-              title=""
-              href=""
-              coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
-              shape="poly"
-            />
-          </map>
+        console.log("onClickItem");
+
+        if (isSelectModalOn) setSelectModalOn(false);
+        else {
+            setSelectModalOn(true);
+            setSelectedItem(item);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="AppWrap">
+                MYAPP
+                <div className="imgWrap">
+                    <div className="roomBgImg" />
+
+                    {/* <div className="bedDefaultImg" useMap="#image-map" /> */}
+                    <img src={bed_default} usemap="#image-map" />
+                    <map name="image-map">
+                        <area
+                            // onClick={(e) => {
+                            //     e.preventDefault();
+                            //     alert("bed");
+                            // }}
+                            onClick={(e) => onClickItem(e, "bed")}
+                            target=""
+                            alt=""
+                            title=""
+                            href=""
+                            coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
+                            shape="poly"
+                        />
+                    </map>
+                </div>
+                {isSelectModalOn && (
+                    <SelectModal
+                        item={selectedItem}
+                        setSelectModalOn={setSelectModalOn}
+                    />
+                )}
+                {/* <SelectModal /> */}
+            </div>
         </div>
-        <div className="selectBox"></div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
