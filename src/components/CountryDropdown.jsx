556ed08e07083445a6b77de191bbe3dbf3f30015
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-27 02:17:58 +0900

    design: select 옆 화살표 아이콘 추가, select 배치 조정
    - select에서 선택된 값을 나타내는 변수명 더 디테일하게 변경
    - select 기본 디자인 제거

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -7,8 +7,8 @@ export default function CountryDropdown() {
 
   const onSelect = (e) => {
     // console.dir(e.target.value);
-    const selectedVal = e.target.value;
-    fetchWeater(selectedVal).then((data) => {
+    const selectedCountry = e.target.value;
+    fetchWeater(selectedCountry).then((data) => {
       console.log("data :>> ", data.weather);
       setWeatherIcon(data.weather[0].icon);
     });
@@ -16,21 +16,22 @@ export default function CountryDropdown() {
 
   return (
     <div className="dropdownWrap">
-      <select
-        name="country"
-        onChange={(e) => onSelect(e)}
-        className="countrySelect"
-      >
-        <option value="Seoul" selected>
-          서울
-        </option>
-        <option value="Tokyo">도쿄</option>
-        <option value="London">런던</option>
-        <option value="Paris">파리</option>
-        <option value="Hong Kong">홍콩</option>
-        <option value="Washington">워싱턴</option>
-        <option value="Singapore">싱가포르</option>
-      </select>
+      <div className="selectWrap">
+        <select name="country" onChange={(e) => onSelect(e)} className="select">
+          <option value="Seoul" selected>
+            서울
+          </option>
+          <option value="Tokyo">도쿄</option>
+          <option value="London">런던</option>
+          <option value="Paris">파리</option>
+          <option value="Hong Kong">홍콩</option>
+          <option value="Washington">워싱턴</option>
+          <option value="Singapore">싱가포르</option>
+        </select>
+        <span className="select__downIcon material-icons-round">
+          expand_more
+        </span>
+      </div>
       <div className="icon">
         <img
           src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
