e7e46d5949038ff17fe369ae8a47a4097a5c4679
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-26 21:12:42 +0900

    feat: 국가 선택할 수 있는 CountryDropdown.jsx 컴포넌트 생성
    - CoutryDropdown.scss 생성
    - App.jsx에 해당 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ImageMap from "image-map";
 import { useEffect, useRef, useState } from "react";
 import bed_default from "./assets/img/bed_default.png";
 import SelectModal from "./components/SelectModal";
+import CountryDropdown from "./components/CountryDropdown";
 
 function App() {
     const [isSelectModalOn, setSelectModalOn] = useState(false);
@@ -27,47 +28,44 @@ function App() {
         }
     };
 
-    return (
-        <div className="App">
-            <div className="AppWrap">
-                MYAPP
-                <div className="imgWrap">
-                    <div className="roomBgImg" />
+  return (
+    <div className="App">
+      <div className="AppWrap">
+        <CountryDropdown />
+        MYAPP
+        <div className="imgWrap">
+          <div className="roomBgImg" />
 
-                    {/* <div className="bedDefaultImg" useMap="#image-map" /> */}
-                    <img
-                        src={require(`./assets/img/${bedImg}.png`)}
-                        usemap="#image-map"
-                        alt="가구"
-                    />
-                    <map name="image-map">
-                        <area
-                            // onClick={(e) => {
-                            //     e.preventDefault();
-                            //     alert("bed");
-                            // }}
-                            onClick={(e) => onClickItem(e, "bed")}
-                            target=""
-                            alt=""
-                            title=""
-                            href=""
-                            coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
-                            shape="poly"
-                        />
-                    </map>
-                </div>
-                {isSelectModalOn && (
-                    <SelectModal
-                        item={selectedItem} //'bed'
-                        setSelectModalOn={setSelectModalOn}
-                        curBedImg={curBedImg.current}
-                        setBedImg={setBedImg}
-                    />
-                )}
-                {/* <SelectModal /> */}
-            </div>
+          {/* <div className="bedDefaultImg" useMap="#image-map" /> */}
+          <img
+            src={require(`./assets/img/${bedImg}.png`)}
+            usemap="#image-map"
+            alt="가구"
+          />
+          <map name="image-map">
+            <area
+              onClick={(e) => onClickItem(e, "bed")}
+              target=""
+              alt=""
+              title=""
+              href=""
+              coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
+              shape="poly"
+            />
+          </map>
         </div>
-    );
+        {isSelectModalOn && (
+          <SelectModal
+            item={selectedItem} //'bed'
+            setSelectModalOn={setSelectModalOn}
+            curBedImg={curBedImg.current}
+            setBedImg={setBedImg}
+          />
+        )}
+        {/* <SelectModal /> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
