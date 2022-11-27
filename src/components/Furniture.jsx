736821d73158ff890ac67fab618ad7b2d5693022
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-27 22:01:22 +0900

    feat: image-map 라이브러리 코드를 각 가구 컴포넌트 대신 RoomPage.jsx 컴포넌트에 넣음
    각 가구 컴포넌트에 image-map 라이브러리 코드를 넣으면 여러 image-map 중 한 개만 작동돼서 RoomPage.jsx로 이동시킴

--- src/components/Furniture.jsx
+++ src/components/Furniture.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import ImageMap from "image-map";
 import "../styles/Furniture.scss";
 
 export default function Furniture({
@@ -12,9 +11,6 @@ export default function Furniture({
   const nickname = localStorage.getItem("nickname");
 
   console.log("furniture : ");
-  useEffect(() => {
-    ImageMap("img[usemap]");
-  }, []);
 
   useEffect(() => {
     console.log("useEffect!");
@@ -24,16 +20,15 @@ export default function Furniture({
       setImg(JSON.parse(localStorage.getItem("db"))[nickname][kind]);
   }, [localStorage.getItem("db")]);
 
-  const onClickItem = (e) => {
-    e.preventDefault();
+  //   const onClickItem = (e) => {
+  //     e.preventDefault();
+  //     console.log("onClickItem");
 
-    console.log("onClickItem");
-
-    if (isSelectModalOn) setSelectModalOn(false);
-    else {
-      setSelectModalOn(kind);
-    }
-  };
+  //     if (isSelectModalOn) setSelectModalOn(false);
+  //     else {
+  //       setSelectModalOn(kind);
+  //     }
+  //   };
   return (
     <div className="furnitureWrap">
       {/* <div className="bedDefaultImg" useMap="#image-map" /> */}
@@ -45,17 +40,6 @@ export default function Furniture({
         usemap="#image-map"
         alt="가구"
       />
-      <map name="image-map">
-        <area
-          onClick={(e) => onClickItem(e)}
-          target=""
-          alt=""
-          title=""
-          href=""
-          coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
-          shape="poly"
-        />
-      </map>
     </div>
   );
 }
--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -5,6 +5,8 @@ import CountryDropdown from "../components/CountryDropdown";
 import "../styles/RoomPage.scss";
 import { useNavigate } from "react-router-dom";
 import Furniture from "../components/Furniture";
+import Window from "../components/Window";
+import ImageMap from "image-map";
 
 export default function RoomPage() {
   const [nickname, setNickname] = useState(localStorage.getItem("nickname"));
@@ -28,7 +30,24 @@ export default function RoomPage() {
       setNickname(localStorage.getItem("nickname"));
     }
   }, []);
+  useEffect(() => {
+    ImageMap("img[usemap]");
+  }, []);
 
+  const onClickItem = (e, kind) => {
+    e.preventDefault();
+    console.log("onClickItem");
+
+    if (isSelectModalOn) setSelectModalOn(false);
+    else {
+      setSelectModalOn(kind);
+    }
+  };
+
+  const onClickWindow = (e, kind) => {
+    console.log("click window");
+    e.preventDefault();
+  };
   return (
     <div className="RoomPageWrap">
       <div style={{ marginLeft: "1rem" }}>
@@ -36,15 +55,17 @@ export default function RoomPage() {
         <p className="title">{nickname} 님의 방</p>
       </div>
       <div className="imgWrap">
-        <img
+        {/* <img
           className="img"
           src={require("../assets/img/window/day.png")}
           alt="창문"
-        />
+        /> */}
+        <Window />
         <img
           className="img"
           src={require("../assets/img/room_bg.png")}
           alt="방"
+          usemap="#image-map"
         />
         {/* <div className="roomBgImg" /> */}
         {/* <div style={{ width: "30px" }}>
@@ -63,6 +84,24 @@ export default function RoomPage() {
           img={bedImg}
           setImg={setBedImg}
         />
+        <map name="image-map">
+          <area
+            onClick={(e) => onClickItem(e, "bed")}
+            target=""
+            alt=""
+            title=""
+            coords="60,533,62,444,75,436,93,444,165,438,165,426,176,422,191,430,189,473,341,545,345,524,359,516,373,526,376,570,363,583,265,634"
+            shape="poly"
+          />
+          <area
+            onClick={(e) => onClickWindow(e, "window")}
+            target=""
+            alt=""
+            title=""
+            coords="123,217,126,413,317,320,318,126"
+            shape="poly"
+          />
+        </map>
       </div>
       {isSelectModalOn && (
         <SelectModal
